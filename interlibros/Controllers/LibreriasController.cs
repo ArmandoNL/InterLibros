e2124ffc487145424c38d45a20036d7a10da52c9
eariassoto <ariassotoemmanuel@gmail.com>
2016-07-10 20:39:05 -0600

    agregadas un par de controladoras |

---
+++ interlibros/Controllers/LibreriasController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using interlibros.Models;
+
+namespace interlibros.Controllers
+{
+    public class LibreriasController : Controller
+    {
+        private interlibrosEntities db = new interlibrosEntities();
+
+        // GET: Librerias
+        public ActionResult Index()
+        {
+            return View(db.Librerias.ToList());
+        }
+
+        // GET: Librerias/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Librerias librerias = db.Librerias.Find(id);
+            if (librerias == null)
+            {
+                return HttpNotFound();
+            }
+            return View(librerias);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ interlibros/Controllers/UsuariosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using interlibros.Models;
+
+namespace interlibros.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private interlibrosEntities db = new interlibrosEntities();
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuarios usuarios = db.Usuarios.Find(id);
+            if (usuarios == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuarios);
+        }
+
+        // todo quitar mas adelante
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
