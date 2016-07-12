19dca40d7f48ae71988ce1ff3d0ab33181026ed9
eariassoto <ariassotoemmanuel@gmail.com>
2016-07-12 15:47:14 -0600

    ver 1.1

--- interlibros/Controllers/HomeController.cs
+++ interlibros/Controllers/HomeController.cs
@@ -49,18 +49,10 @@ namespace interlibros.Controllers
             return View(viewModel);
         }
 
-        public ActionResult About()
+        public ActionResult Contactenos()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
